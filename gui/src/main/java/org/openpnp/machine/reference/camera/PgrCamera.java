3161637e9e0758e9da2c4eeaa41418bc4280b4e5
Johannes Taelman <johannes.taelman@gmail.com>
2013-12-14 19:39:15 +0100

    added point grey camera support

---
+++ gui/src/main/java/org/openpnp/machine/reference/camera/PgrCamera.java
@@ -0,0 +1,140 @@
+package org.openpnp.machine.reference.camera;
+
+import com.pointgrey.api.PGCameraMode;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openpnp.ConfigurationListener;
+import org.openpnp.gui.support.Wizard;
+import org.openpnp.machine.reference.ReferenceCamera;
+import org.openpnp.model.Configuration;
+import org.simpleframework.xml.Attribute;
+
+import static com.pointgrey.api.PointGreyCameraInterface.connectToDefaultCamera;
+import static com.pointgrey.api.PointGreyCameraInterface.createContext;
+import static com.pointgrey.api.PointGreyCameraInterface.getCameraName;
+import static com.pointgrey.api.PointGreyCameraInterface.getNumOfCameras;
+import static com.pointgrey.api.PointGreyCameraInterface.getSupportedCameraModes;
+import static com.pointgrey.api.PointGreyCameraInterface.startCapture;
+import static com.pointgrey.api.PointGreyCameraInterface.setCameraMode;
+import static com.pointgrey.api.PointGreyCameraInterface.storeImage;
+import java.awt.image.DataBuffer;
+import java.awt.image.DataBufferByte;
+import java.awt.image.Raster;
+import java.awt.image.SampleModel;
+import java.awt.image.WritableRaster;
+import org.openpnp.machine.reference.camera.wizards.PgrCameraConfigurationWizard;
+
+/**
+ *
+ * @author jtaelman
+ */
+
+public class PgrCamera extends ReferenceCamera implements Runnable {
+    private BufferedImage lastImage;
+    private Object captureLock = new Object();
+    private Thread captureThread;
+    @Attribute(required=false)
+    private String driver;
+
+    public PgrCamera() {
+    Configuration.get().addListener(new ConfigurationListener.Adapter() {
+
+        @Override
+        public void configurationLoaded(Configuration configuration)
+                throws Exception {
+            if (driver != null && driver.trim().length() != 0) {
+                setDriver(driver);
+            }
+        }
+    });
+    }
+
+    public String getDriver() {
+            return driver;
+    }
+
+    public void setDriver(String driver) {
+            if (captureThread != null) {
+                    captureThread.interrupt();
+                    try {
+                            captureThread.join();
+                    }
+                    catch (Exception e) {
+                            e.printStackTrace();
+                    }
+            }
+            this.driver = driver;
+            captureThread = new Thread(this);
+            captureThread.start();
+    }
+    
+    
+    @Override
+    public BufferedImage capture() {
+            synchronized (captureLock) {
+                    try {
+                            captureLock.wait();
+                            BufferedImage image = lastImage;
+                            return image;
+                    }
+                    catch (Exception e) {
+                            e.printStackTrace();
+                            return null;
+                    }
+            }
+    }
+
+    @Override
+    public Wizard getConfigurationWizard() {
+            return new PgrCameraConfigurationWizard(this);
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Creating a context for the camera.");
+        createContext();
+        
+        System.out.println("\nThere are " + getNumOfCameras() + " camera(s) connected to the system");
+        System.out.println("Connecting to the default camera.");
+        connectToDefaultCamera();
+        System.out.println("Connected to: " + getCameraName());
+        
+        System.out.println("\nStarting capture.");
+        startCapture();
+
+        PGCameraMode mode = getSupportedCameraModes()[0];
+        System.out.println("Capturing an image with mode: " + mode );
+
+        //Changing the camera mode will cause the camera to stop capturing, it must be started again.
+        setCameraMode(mode);
+        startCapture();
+
+        int width = mode.getVideoMode().getWidth();
+        int height = mode.getVideoMode().getHeight();
+        while (!Thread.interrupted()) {
+            byte[] buff = new byte[width*height*3];
+            storeImage(buff);
+            // assuming Y8 !
+            BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
+            WritableRaster raster = img.getRaster();
+//            WritableRaster raster = Raster.createPackedRaster(DataBuffer.TYPE_BYTE, width, height, 1, 8, null);
+            raster.setDataElements(0, 0, width, height, buff);
+//raster.setPixels(0, 0, width, height, buff);
+            img.setData(raster);
+//            DataBufferByte db = (DataBufferByte)img.getRaster().getDataBuffer();
+//            System.out.println("db size : "+ db.getSize());
+            broadcastCapture(lastImage = img);
+            synchronized (captureLock) {
+                    captureLock.notify();
+            }
+            try {
+                    Thread.sleep(1000 / 30);
+            }
+            catch (Exception e) {
+            }
+        }            
+    }
+    
+}
---
+++ gui/src/main/java/org/openpnp/machine/reference/camera/wizards/PgrCameraConfigurationWizard.java
@@ -0,0 +1,177 @@
+/*
+ 	Copyright (C) 2011 Jason von Nieda <jason@vonnieda.org>
+ 	
+ 	This file is part of OpenPnP.
+ 	
+	OpenPnP is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenPnP is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenPnP.  If not, see <http://www.gnu.org/licenses/>.
+ 	
+ 	For more information about OpenPnP visit http://openpnp.org
+*/
+
+package org.openpnp.machine.reference.camera.wizards;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
+
+import org.openpnp.gui.support.ApplyResetBindingListener;
+import org.openpnp.gui.support.JBindings;
+import org.openpnp.gui.support.JBindings.WrappedBinding;
+import org.openpnp.gui.support.Wizard;
+import org.openpnp.gui.support.WizardContainer;
+import org.openpnp.machine.reference.camera.PgrCamera;
+
+import com.jgoodies.forms.factories.FormFactory;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.RowSpec;
+
+public class PgrCameraConfigurationWizard extends JPanel implements Wizard {
+	private final PgrCamera camera;
+
+	private WizardContainer wizardContainer;
+	private JButton btnSave;
+	private JButton btnCancel;
+	private JPanel panelGeneral;
+
+	private List<WrappedBinding> wrappedBindings = new ArrayList<WrappedBinding>();
+
+	public PgrCameraConfigurationWizard(
+			PgrCamera camera) {
+		this.camera = camera;
+
+		setLayout(new BorderLayout());
+
+		JPanel panelFields = new JPanel();
+		panelFields.setLayout(new BoxLayout(panelFields, BoxLayout.Y_AXIS));
+
+		JScrollPane scrollPane = new JScrollPane(panelFields);
+
+		panelGeneral = new JPanel();
+		panelFields.add(panelGeneral);
+		panelGeneral.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null), "General", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panelGeneral.setLayout(new FormLayout(new ColumnSpec[] {
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("default:grow"),},
+			new RowSpec[] {
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,}));
+		
+		JLabel lblDeviceId = new JLabel("Driver");
+		panelGeneral.add(lblDeviceId, "2, 2, right, default");
+		
+                String[] deviceIds = {"default"};
+
+                comboBoxDriver = new JComboBox(deviceIds);
+		panelGeneral.add(comboBoxDriver, "4, 2, left, default");
+		
+		scrollPane.setBorder(null);
+		add(scrollPane, BorderLayout.CENTER);
+
+		JPanel panelActions = new JPanel();
+		panelActions.setLayout(new FlowLayout(FlowLayout.RIGHT));
+		add(panelActions, BorderLayout.SOUTH);
+
+		btnCancel = new JButton(cancelAction);
+		panelActions.add(btnCancel);
+		
+		btnSave = new JButton(saveAction);
+		panelActions.add(btnSave);
+
+		createBindings();
+		loadFromModel();
+	}
+
+	private void createBindings() {
+		ApplyResetBindingListener listener = new ApplyResetBindingListener(saveAction, cancelAction);
+		
+		// The order of the properties is important. We want all the booleans
+		// to be set before we set the driver because setting the driver
+		// applies all the settings.
+		wrappedBindings.add(JBindings.bind(camera, "driver",
+				comboBoxDriver, "selectedItem", listener));
+	}
+
+	private void loadFromModel() {
+		for (WrappedBinding wrappedBinding : wrappedBindings) {
+			wrappedBinding.reset();
+		}
+		saveAction.setEnabled(false);
+		cancelAction.setEnabled(false);
+	}
+
+	private void saveToModel() {
+		for (WrappedBinding wrappedBinding : wrappedBindings) {
+			wrappedBinding.save();
+		}
+		saveAction.setEnabled(false);
+		cancelAction.setEnabled(false);
+	}
+
+	@Override
+	public void setWizardContainer(WizardContainer wizardContainer) {
+		this.wizardContainer = wizardContainer;
+	}
+
+	@Override
+	public JPanel getWizardPanel() {
+		return this;
+	}
+
+	@Override
+	public String getWizardName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private Action saveAction = new AbstractAction("Apply") {
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			saveToModel();
+			wizardContainer
+					.wizardCompleted(PgrCameraConfigurationWizard.this);
+		}
+	};
+
+	private Action cancelAction = new AbstractAction("Reset") {
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			loadFromModel();
+		}
+	};
+	private JComboBox comboBoxDriver;
+}
